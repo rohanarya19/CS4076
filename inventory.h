75eabf32075b50dcf65c8abca653a1b0b5cbda29
Rohan Arya <91119895+rohanarya19@users.noreply.github.com>
2022-04-06 04:05:30 +0100

    Add files via upload

---
+++ inventory.h
@@ -0,0 +1,17 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include "item.h"
+class Inventory
+{
+    private:
+        vector<Item*> itemsInInventory;
+    public:
+        Inventory();
+        void showInventory();
+        void addToInventory(Item *item);
+        void removeFromInventory(Item item);
+        int inventoryQty();
+        vector<Item *> getInventoryList();
+        void setInventoryList(vector<Item *> InventoryList);
+};
+#endif // INVENTORY_H
