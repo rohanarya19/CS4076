fd653237553ffc77dabc2547e8ecced8fa46586a
Rohan Arya <91119895+rohanarya19@users.noreply.github.com>
2022-04-07 22:46:25 +0100

    Add files via upload

---
+++ moc_restaurant.cpp
@@ -0,0 +1,204 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'restaurant.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../restaurantzork/restaurant.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'restaurant.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_restaurant_t {
+    const uint offsetsAndSize[42];
+    char stringdata0[383];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_restaurant_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_restaurant_t qt_meta_stringdata_restaurant = {
+    {
+QT_MOC_LITERAL(0, 10), // "restaurant"
+QT_MOC_LITERAL(11, 18), // "on_button1_clicked"
+QT_MOC_LITERAL(30, 0), // ""
+QT_MOC_LITERAL(31, 18), // "on_button2_clicked"
+QT_MOC_LITERAL(50, 18), // "on_button3_clicked"
+QT_MOC_LITERAL(69, 18), // "on_button4_clicked"
+QT_MOC_LITERAL(88, 18), // "on_button5_clicked"
+QT_MOC_LITERAL(107, 18), // "on_button6_clicked"
+QT_MOC_LITERAL(126, 18), // "on_button7_clicked"
+QT_MOC_LITERAL(145, 18), // "on_button8_clicked"
+QT_MOC_LITERAL(164, 18), // "on_button9_clicked"
+QT_MOC_LITERAL(183, 19), // "on_button10_clicked"
+QT_MOC_LITERAL(203, 19), // "on_button11_clicked"
+QT_MOC_LITERAL(223, 19), // "on_button12_clicked"
+QT_MOC_LITERAL(243, 19), // "on_button13_clicked"
+QT_MOC_LITERAL(263, 19), // "on_button15_clicked"
+QT_MOC_LITERAL(283, 19), // "on_button16_clicked"
+QT_MOC_LITERAL(303, 19), // "on_button17_clicked"
+QT_MOC_LITERAL(323, 19), // "on_button18_clicked"
+QT_MOC_LITERAL(343, 19), // "on_button19_clicked"
+QT_MOC_LITERAL(363, 19) // "on_button20_clicked"
+
+    },
+    "restaurant\0on_button1_clicked\0\0"
+    "on_button2_clicked\0on_button3_clicked\0"
+    "on_button4_clicked\0on_button5_clicked\0"
+    "on_button6_clicked\0on_button7_clicked\0"
+    "on_button8_clicked\0on_button9_clicked\0"
+    "on_button10_clicked\0on_button11_clicked\0"
+    "on_button12_clicked\0on_button13_clicked\0"
+    "on_button15_clicked\0on_button16_clicked\0"
+    "on_button17_clicked\0on_button18_clicked\0"
+    "on_button19_clicked\0on_button20_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_restaurant[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      19,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  128,    2, 0x0a,    1 /* Public */,
+       3,    0,  129,    2, 0x0a,    2 /* Public */,
+       4,    0,  130,    2, 0x0a,    3 /* Public */,
+       5,    0,  131,    2, 0x0a,    4 /* Public */,
+       6,    0,  132,    2, 0x0a,    5 /* Public */,
+       7,    0,  133,    2, 0x0a,    6 /* Public */,
+       8,    0,  134,    2, 0x0a,    7 /* Public */,
+       9,    0,  135,    2, 0x0a,    8 /* Public */,
+      10,    0,  136,    2, 0x0a,    9 /* Public */,
+      11,    0,  137,    2, 0x0a,   10 /* Public */,
+      12,    0,  138,    2, 0x0a,   11 /* Public */,
+      13,    0,  139,    2, 0x0a,   12 /* Public */,
+      14,    0,  140,    2, 0x0a,   13 /* Public */,
+      15,    0,  141,    2, 0x0a,   14 /* Public */,
+      16,    0,  142,    2, 0x0a,   15 /* Public */,
+      17,    0,  143,    2, 0x0a,   16 /* Public */,
+      18,    0,  144,    2, 0x0a,   17 /* Public */,
+      19,    0,  145,    2, 0x0a,   18 /* Public */,
+      20,    0,  146,    2, 0x0a,   19 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void restaurant::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<restaurant *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_button1_clicked(); break;
+        case 1: _t->on_button2_clicked(); break;
+        case 2: _t->on_button3_clicked(); break;
+        case 3: _t->on_button4_clicked(); break;
+        case 4: _t->on_button5_clicked(); break;
+        case 5: _t->on_button6_clicked(); break;
+        case 6: _t->on_button7_clicked(); break;
+        case 7: _t->on_button8_clicked(); break;
+        case 8: _t->on_button9_clicked(); break;
+        case 9: _t->on_button10_clicked(); break;
+        case 10: _t->on_button11_clicked(); break;
+        case 11: _t->on_button12_clicked(); break;
+        case 12: _t->on_button13_clicked(); break;
+        case 13: _t->on_button15_clicked(); break;
+        case 14: _t->on_button16_clicked(); break;
+        case 15: _t->on_button17_clicked(); break;
+        case 16: _t->on_button18_clicked(); break;
+        case 17: _t->on_button19_clicked(); break;
+        case 18: _t->on_button20_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject restaurant::staticMetaObject = { {
+    QMetaObject::SuperData::link<QGraphicsScene::staticMetaObject>(),
+    qt_meta_stringdata_restaurant.offsetsAndSize,
+    qt_meta_data_restaurant,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_restaurant_t
+, QtPrivate::TypeAndForceComplete<restaurant, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *restaurant::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *restaurant::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_restaurant.stringdata0))
+        return static_cast<void*>(this);
+    return QGraphicsScene::qt_metacast(_clname);
+}
+
+int restaurant::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGraphicsScene::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 19)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 19;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 19)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 19;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
